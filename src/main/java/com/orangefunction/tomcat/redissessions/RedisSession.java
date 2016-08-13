b507d60950a485e23ba8d7d5bdc8a67479ce08dd
yangjh5 <yangjh5@asiainfo.com>
2016-08-13 14:08:35 +0800

    修改代码兼容使能支持tomcat8.

--- src/main/java/com/orangefunction/tomcat/redissessions/RedisSession.java
+++ src/main/java/com/orangefunction/tomcat/redissessions/RedisSession.java
@@ -1,120 +1,117 @@
 package com.orangefunction.tomcat.redissessions;
 
-import java.security.Principal;
 import org.apache.catalina.Manager;
 import org.apache.catalina.session.StandardSession;
-import java.util.HashMap;
-import java.io.IOException;
-
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.HashMap;
 
 
 public class RedisSession extends StandardSession {
 
-  private final Log log = LogFactory.getLog(RedisSession.class);
-
-  protected static Boolean manualDirtyTrackingSupportEnabled = false;
-
-  public static void setManualDirtyTrackingSupportEnabled(Boolean enabled) {
-    manualDirtyTrackingSupportEnabled = enabled;
-  }
-
-  protected static String manualDirtyTrackingAttributeKey = "__changed__";
-
-  public static void setManualDirtyTrackingAttributeKey(String key) {
-    manualDirtyTrackingAttributeKey = key;
-  }
+    protected static Boolean manualDirtyTrackingSupportEnabled = false;
+    protected static String manualDirtyTrackingAttributeKey = "__changed__";
+    private final Log log = LogFactory.getLog(RedisSession.class);
+    protected HashMap<String, Object> changedAttributes;
+    protected Boolean dirty;
 
 
-  protected HashMap<String, Object> changedAttributes;
-  protected Boolean dirty;
-
-  public RedisSession(Manager manager) {
-    super(manager);
-    resetDirtyTracking();
-  }
-
-  public Boolean isDirty() {
-    return dirty || !changedAttributes.isEmpty();
-  }
-
-  public HashMap<String, Object> getChangedAttributes() {
-    return changedAttributes;
-  }
-
-  public void resetDirtyTracking() {
-    changedAttributes = new HashMap<>();
-    dirty = false;
-  }
-
-  @Override
-  public void setAttribute(String key, Object value) {
-    if (manualDirtyTrackingSupportEnabled && manualDirtyTrackingAttributeKey.equals(key)) {
-      dirty = true;
-      return;
+    public RedisSession(Manager manager) {
+        super(manager);
+        resetDirtyTracking();
     }
 
-    Object oldValue = getAttribute(key);
-    super.setAttribute(key, value);
+    public static void setManualDirtyTrackingSupportEnabled(Boolean enabled) {
+        manualDirtyTrackingSupportEnabled = enabled;
+    }
 
-    if ( (value != null || oldValue != null)
-         && ( value == null && oldValue != null
-              || oldValue == null && value != null
-              || !value.getClass().isInstance(oldValue)
-              || !value.equals(oldValue) ) ) {
-      if (this.manager instanceof RedisSessionManager
-          && ((RedisSessionManager)this.manager).getSaveOnChange()) {
-        try {
-          ((RedisSessionManager)this.manager).save(this, true);
-        } catch (IOException ex) {
-          log.error("Error saving session on setAttribute (triggered by saveOnChange=true): " + ex.getMessage());
+    public static void setManualDirtyTrackingAttributeKey(String key) {
+        manualDirtyTrackingAttributeKey = key;
+    }
+
+    public Boolean isDirty() {
+        return dirty || !changedAttributes.isEmpty();
+    }
+
+    public HashMap<String, Object> getChangedAttributes() {
+        return changedAttributes;
+    }
+
+    public void resetDirtyTracking() {
+        changedAttributes = new HashMap<>();
+        dirty = false;
+    }
+
+    @Override
+    public void setAttribute(String key, Object value) {
+        if (manualDirtyTrackingSupportEnabled && manualDirtyTrackingAttributeKey.equals(key)) {
+            dirty = true;
+            return;
         }
-      } else {
-        changedAttributes.put(key, value);
-      }
+
+        Object oldValue = getAttribute(key);
+        super.setAttribute(key, value);
+
+        if ((value != null || oldValue != null)
+                && (value == null && oldValue != null
+                || oldValue == null && value != null
+                || !value.getClass().isInstance(oldValue)
+                || !value.equals(oldValue))) {
+            if (this.manager instanceof RedisSessionManager
+                    && ((RedisSessionManager) this.manager).getSaveOnChange()) {
+                try {
+                    ((RedisSessionManager) this.manager).save(this, true);
+                } catch (IOException ex) {
+                    log.error("Error saving session on setAttribute (triggered by saveOnChange=true): " + ex.getMessage());
+                }
+            } else {
+                changedAttributes.put(key, value);
+            }
+        }
     }
-  }
 
-  @Override
-  public void removeAttribute(String name) {
-    super.removeAttribute(name);
-    if (this.manager instanceof RedisSessionManager
-        && ((RedisSessionManager)this.manager).getSaveOnChange()) {
-      try {
-        ((RedisSessionManager)this.manager).save(this, true);
-      } catch (IOException ex) {
-        log.error("Error saving session on setAttribute (triggered by saveOnChange=true): " + ex.getMessage());
-      }
-    } else {
-      dirty = true;
+    @Override
+    public void removeAttribute(String name) {
+        super.removeAttribute(name);
+        if (this.manager instanceof RedisSessionManager
+                && ((RedisSessionManager) this.manager).getSaveOnChange()) {
+            try {
+                ((RedisSessionManager) this.manager).save(this, true);
+            } catch (IOException ex) {
+                log.error("Error saving session on setAttribute (triggered by saveOnChange=true): " + ex.getMessage());
+            }
+        } else {
+            dirty = true;
+        }
     }
-  }
 
-  @Override
-  public void setId(String id) {
-    // Specifically do not call super(): it's implementation does unexpected things
-    // like calling manager.remove(session.id) and manager.add(session).
+    @Override
+    public void setId(String id) {
+        // Specifically do not call super(): it's implementation does unexpected things
+        // like calling manager.remove(session.id) and manager.add(session).
 
-    this.id = id;
-  }
+        this.id = id;
+    }
 
-  @Override
-  public void setPrincipal(Principal principal) {
-    dirty = true;
-    super.setPrincipal(principal);
-  }
+    @Override
+    public void setPrincipal(Principal principal) {
+        dirty = true;
+        super.setPrincipal(principal);
+    }
 
-  @Override
-  public void writeObjectData(java.io.ObjectOutputStream out) throws IOException {
-    super.writeObjectData(out);
-    out.writeLong(this.getCreationTime());
-  }
+    @Override
+    public void writeObjectData(java.io.ObjectOutputStream out) throws IOException {
+        super.writeObjectData(out);
+        out.writeLong(this.getCreationTime());
+    }
 
-  @Override
-  public void readObjectData(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
-    super.readObjectData(in);
-    this.setCreationTime(in.readLong());
-  }
+    @Override
+    public void readObjectData(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+        super.readObjectData(in);
+        this.setCreationTime(in.readLong());
+    }
 
 }
